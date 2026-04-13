9d88bf4a82790f771a79d7d1dad92aebd2b1db04
Phillipml <contato.phillip.menezes@gmail.com>
2026-04-13 15:44:37 -0300

    database.py created

---
+++ app/db/database.py
@@ -0,0 +1,9 @@
+import sqlalchemy as sa
+import databases
+from core.settings import get_settings
+
+database = databases.Database(get_settings().DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(
+    get_settings().DATABASE_URL, connect_args={"check_same_thread": False}
+)
