a8a32dee4bef0b75849ea438ae6855b72057435a
Phillipml <contato.phillip.menezes@gmail.com>
2026-04-13 14:50:27 -0300

    settings/core created

--- app/core/settings.py
+++ app/core/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite:///./bank.db"
 
 
 @lru_cache
