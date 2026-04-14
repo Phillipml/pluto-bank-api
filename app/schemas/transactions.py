e57ecc21188e5ee020e9dbcc64a041f48ca25cf7
Phillipml <contato.phillip.menezes@gmail.com>
2026-04-14 10:31:36 -0300

    transactions schema created

--- app/schemas/transactions.py
+++ app/schemas/transactions.py
@@ -1,14 +1,17 @@
+from decimal import Decimal
 from pydantic import BaseModel, Field
+
+from app.schemas.users import UserResponse
 
 
 class TransactionCreate(BaseModel):
-    user_id: int
-    value: int
+    value: Decimal = Field(gt=-1, max_digits=12, decimal_places=2)
     description: str = Field(min_length=3, max_length=255)
 
 
 class TransactionsResponse(BaseModel):
     id: int
     user_id: int
-    value: int
+    value: Decimal = Field(gt=-1, max_digits=12, decimal_places=2)
     description: str
+    user: UserResponse
