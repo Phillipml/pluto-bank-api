22ad595846898663258976c24a186fcf85099e2f
Phillipml <contato.phillip.menezes@gmail.com>
2026-04-14 10:55:44 -0300

    models transactions created

---
+++ app/models/transaction.py
@@ -0,0 +1,13 @@
+import sqlalchemy as sa
+from app.db.database import metadata
+
+transactions = sa.Table(
+    "transactions",
+    metadata,
+    sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
+    sa.Column(
+        "user_id", sa.Integer, sa.ForeignKey("users.id"), nullable=False, index=True
+    ),
+    sa.Column("value", sa.Integer, nullable=False),
+    sa.Column("description", sa.String(255), nullable=False, server_default=""),
+)
