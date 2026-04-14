ffe03a8dcbbd50270797568532142d65bd3539e5
Phillipml <contato.phillip.menezes@gmail.com>
2026-04-14 12:00:24 -0300

    transactions controller

--- app/schemas/transactions.py
+++ app/schemas/transactions.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from app.schemas.users import UserResponse
 
 
-class TransactionCreate(BaseModel):
+class TransactionRequest(BaseModel):
     value: Decimal = Field(gt=-1, max_digits=12, decimal_places=2)
     description: str = Field(min_length=3, max_length=255)
 
